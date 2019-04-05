97f9e6e624bf68c31d5a39ccb2063db0e8e013a6
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-04-05 18:50:46 -0300

    Creates Profile for test (H2) and dev (MySQL) datasources

--- src/main/java/br/com/cinq/spring/data/sample/application/SpringjpaApplication.java
+++ src/main/java/br/com/cinq/spring/data/sample/application/SpringjpaApplication.java
@@ -1,25 +1,11 @@
 package br.com.cinq.spring.data.sample.application;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.cinq.spring.data.sample.application.domain.City;
-import br.com.cinq.spring.data.sample.application.domain.Country;
-import br.com.cinq.spring.data.sample.application.repositories.CityRepository;
-import br.com.cinq.spring.data.sample.application.repositories.CountryRepository;
-
 @SpringBootApplication
 public class SpringjpaApplication implements CommandLineRunner {
-
-	@Autowired
-	private CityRepository cityRepository;
-	
-	@Autowired
-	private CountryRepository countryRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringjpaApplication.class, args);
@@ -29,32 +15,6 @@ public class SpringjpaApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		Country country1 = new Country(null, "Brazil");
-		Country country2 = new Country(null, "Argentina");
-		Country country3 = new Country(null, "Netherlands");
-		Country country4 = new Country(null, "Ireland");
-		Country country5 = new Country(null, "Norway");
-		Country country6 = new Country(null, "Czech Republic");
-		
-		
-		City city1 = new City(null, "Andirá", country1);
-		City city2 = new City(null, "Curitiba", country1);
-		City city3 = new City(null, "Londrina", country1);
-		City city4 = new City(null, "Buenos Aires", country2);
-		City city5 = new City(null, "Amsterdam", country3);
-		City city6 = new City(null, "Leewarden", country3);
-		
-		City city7 = new City(null, "Dublin", country4);
-		City city8 = new City(null, "Limerick", country4);
-		City city9 = new City(null, "Galway", country4);
-		City city10 = new City(null, "Cork", country4);
-		City city11 = new City(null, "Oslo", country5);
-		City city12 = new City(null, "Bergen", country5);
-		City city13 = new City(null, "Trondheim", country5);
-		City city14 = new City(null, "Prague", country6);
-		
-		countryRepository.saveAll(Arrays.asList(country1, country2, country3, country4, country5, country6));
-		cityRepository.saveAll(Arrays.asList(city1,city2,city3,city4,city5,city6,city7,city8,city9,city10,city11,city12,city13,city14));
 		
 	}
 
---
+++ src/main/java/br/com/cinq/spring/data/sample/application/config/TestConfig.java
@@ -0,0 +1,29 @@
+package br.com.cinq.spring.data.sample.application.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.cinq.spring.data.sample.application.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantiateTestDB();
+		return true;
+	}
+}
